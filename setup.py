bbb5e55f832aa9d8c10d86626e40d1855de94879
Karina Zadorozhny <karinazad@dhcp-10-105-193-23.wireless.northwestern.private>
2022-03-08 08:10:02 -0600

    Refactor

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
-import setuptools
+# import setuptools
+# from setuptools import setup, find_packages
+#
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+#
+# setuptools.setup(
+#     name='selecting_ood_detector',
+#     version='0.1',
+#     author='Pacmed BV',
+#     description='Out-of-distribution detection for Tabular Data',
+#     long_description=long_description,
+#
+#     long_description_content_type="text/markdown",
+#     url='https://github.com/Giovannicina/selecting_OOD_detector',
+#     license='MIT',
+#     # packages=['selecting_ood_detector'],
+#     packages=find_packages(),
+#     install_requires=['requests'],
+# )
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import codecs
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name='selecting_ood_detector',
-    version='0.0.0',
-    author='Pacmed BV',
+with codecs.open("README.md", encoding="utf-8") as f:
+    README = f.read()
+
+
+setup(
+    name="selecting_ood_detector",
     description='Out-of-distribution detection for Tabular Data',
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
-    url='https://github.com/Giovannicina/selecting_OOD_detector',
-    license='MIT',
-    packages=['selecting_ood_detector'],
-    install_requires=['requests'],
+    version="0.1",
+    packages=find_packages(),
+    author='Pacmed BV',
+    url="https://github.com/y0ast/DUE",
+    # author_email="joost.van.amersfoort@cs.ox.ac.uk",
+    # install_requires=["gpytorch>=1.2.1", "torch", "scikit-learn"],
+    python_requires=">=3.6",
 )
